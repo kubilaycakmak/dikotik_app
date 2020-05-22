8388caae68d6c794e6aeb44ad5085d41cb3d5694
Kubilay Çakmak <31804832+kubilayckmk@users.noreply.github.com>
2020-05-22 17:03:53 +0300

    second part

--- lib/pages/entry_page.dart
+++ lib/pages/entry_page.dart
@@ -3,6 +3,7 @@ import 'package:dikotik_app/pages/style/background.dart';
 import 'package:dikotik_app/pages/style/text_style.dart';
 import 'package:flutter/material.dart';
 
+import 'audiometry_page.dart';
 
 class EntryPage extends StatelessWidget {
   @override
@@ -18,7 +19,6 @@ class EntryPage extends StatelessWidget {
     );
   }
 
-
   Widget buildEntryScreen(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 120),
@@ -30,48 +30,54 @@ class EntryPage extends StatelessWidget {
               child: Image(
                 width: 200,
                 color: Colors.white70,
-                  image: AssetImage('assets/images/audio.png', ),
+                image: AssetImage(
+                  'assets/images/audio.png',
                 ),
+              ),
             ),
             Container(
               padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
               child: Text(
-                'DİKOTİK DİNLEME TESTLERİ',
+                'DİKOTİK DİNLEME',
                 style: titleText,
               ),
             ),
             SizedBox(
               height: 10,
             ),
-            buildButtons(context, 'BAŞLA'),
+            buildButtons(context, 'AUDIOMETRY', 0),
+            SizedBox(
+              height: 10,
+            ),
+            buildButtons(context, 'DİKOTİK DİNLEME TESTLERİ', 1),
           ],
         ),
       ),
     );
   }
 
-
-  Widget buildButtons(BuildContext context, String title) {
+  Widget buildButtons(BuildContext context, String title, int index) {
     return Container(
       width: 300,
       padding: EdgeInsets.symmetric(horizontal: 20),
-          child: FloatingActionButton.extended(
-            backgroundColor: Colors.lightBlue.shade900,
-            heroTag: '$title',
-            focusColor: Colors.transparent,
-            hoverColor: Colors.transparent,
-            splashColor: Colors.transparent,
-            label: Text(
-              title,
-              style: paragraphText,
-            ),
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => GetInformationPage()));
-            },
-          ),
-        );
+      child: FloatingActionButton.extended(
+        backgroundColor: Colors.lightBlue.shade900,
+        heroTag: '$title',
+        focusColor: Colors.transparent,
+        hoverColor: Colors.transparent,
+        splashColor: Colors.transparent,
+        label: Text(
+          title,
+          style: paragraphText,
+        ),
+        onPressed: () {
+          index == 1
+              ? Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => GetInformationPage()))
+              : Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => AudiometryPage()));
+        },
+      ),
+    );
   }
 }
